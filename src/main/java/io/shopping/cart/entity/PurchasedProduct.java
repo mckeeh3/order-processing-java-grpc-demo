10fd1cff82f5423d38e6c37fbe64adc9535570bc
mckeeh3 <mckeeh3@gmail.com>
2022-01-07 11:32:53 -0500

    clean up after sync

--- src/main/java/io/shopping/cart/entity/PurchasedProduct.java
+++ src/main/java/io/shopping/cart/entity/PurchasedProduct.java
@@ -2,41 +2,29 @@ package io.shopping.cart.entity;
 
 import com.akkaserverless.javasdk.valueentity.ValueEntityContext;
 import com.google.protobuf.Empty;
-
 import io.shopping.cart.api.PurchasedProductApi;
-import io.shopping.cart.entity.PurchasedProductEntity.PurchasedProductState;
 
 // This class was initially generated based on the .proto definition by Akka Serverless tooling.
 //
 // As long as this file exists it will not be overwritten: you can maintain it yourself,
 // or delete it so it is regenerated as needed.
 
+/** A value entity. */
 public class PurchasedProduct extends AbstractPurchasedProduct {
+  @SuppressWarnings("unused")
+  private final String entityId;
 
   public PurchasedProduct(ValueEntityContext context) {
+    this.entityId = context.entityId();
   }
 
   @Override
   public PurchasedProductEntity.PurchasedProductState emptyState() {
-    return PurchasedProductEntity.PurchasedProductState.getDefaultInstance();
+    throw new UnsupportedOperationException("Not implemented yet, replace with your empty entity state");
   }
 
   @Override
-  public Effect<Empty> addPurchasedProduct(PurchasedProductEntity.PurchasedProductState state, PurchasedProductApi.PurchasedProduct command) {
-    return effects()
-        .updateState(updateState(state, command))
-        .thenReply(Empty.getDefaultInstance());
-  }
-
-  private PurchasedProductState updateState(PurchasedProductEntity.PurchasedProductState state, PurchasedProductApi.PurchasedProduct command) {
-    return state
-        .toBuilder()
-        .setCustomerId(command.getCustomerId())
-        .setCartId(command.getCartId())
-        .setProductId(command.getProductId())
-        .setProductName(command.getProductName())
-        .setQuantity(command.getQuantity())
-        .setPurchasedUtc(command.getPurchasedUtc())
-        .build();
+  public Effect<Empty> addPurchasedProduct(PurchasedProductEntity.PurchasedProductState currentState, PurchasedProductApi.PurchasedProduct purchasedProduct) {
+    return effects().error("The command handler for `AddPurchasedProduct` is not implemented, yet");
   }
 }
--- src/main/java/io/shopping/cart/view/PurchasedProductsView.java
+++ src/main/java/io/shopping/cart/view/PurchasedProductsView.java
@@ -1,14 +1,9 @@
 package io.shopping.cart.view;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import com.akkaserverless.javasdk.view.ViewContext;
-import com.google.protobuf.Any;
-import com.google.protobuf.Timestamp;
-
+import com.akkaserverless.javasdk.view.View;
 import io.shopping.cart.entity.PurchasedProductEntity;
+import com.google.protobuf.Any;
 
 // This class was initially generated based on the .proto definition by Akka Serverless tooling.
 //
@@ -16,35 +11,23 @@ import io.shopping.cart.entity.PurchasedProductEntity;
 // or delete it so it is regenerated as needed.
 
 public class PurchasedProductsView extends AbstractPurchasedProductsView {
-  private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 
-  public PurchasedProductsView(ViewContext context) {
-  }
+  public PurchasedProductsView(ViewContext context) {}
 
   @Override
   public PurchasedProductsOuter.PurchasedProduct emptyState() {
-    return PurchasedProductsOuter.PurchasedProduct.getDefaultInstance();
+    throw new UnsupportedOperationException("Not implemented yet, replace with your empty view state");
   }
 
   @Override
-  public UpdateEffect<PurchasedProductsOuter.PurchasedProduct> processItemCheckedOut(PurchasedProductsOuter.PurchasedProduct state, PurchasedProductEntity.PurchasedProductState command) {
-    return effects().updateState(
-        state.toBuilder()
-            .setCustomerId(command.getCustomerId())
-            .setCartId(command.getCartId())
-            .setProductId(command.getProductId())
-            .setProductName(command.getProductName())
-            .setQuantity(command.getQuantity())
-            .setPurchasedUtc(toUtc(command.getPurchasedUtc()))
-            .build());
+  public View.UpdateEffect<PurchasedProductsOuter.PurchasedProduct> processItemCheckedOut(
+    PurchasedProductsOuter.PurchasedProduct state, PurchasedProductEntity.PurchasedProductState purchasedProductState) {
+    throw new UnsupportedOperationException("Update handler for 'ProcessItemCheckedOut' not implemented yet");
   }
-
   @Override
-  public UpdateEffect<PurchasedProductsOuter.PurchasedProduct> ignoreOtherEvents(PurchasedProductsOuter.PurchasedProduct state, Any any) {
-    return effects().ignore();
-  }
-
-  static String toUtc(Timestamp timestamp) {
-    return dateFormat.format(new Date(timestamp.getSeconds() * 1000 + timestamp.getNanos() / 1000000));
+  public View.UpdateEffect<PurchasedProductsOuter.PurchasedProduct> ignoreOtherEvents(
+    PurchasedProductsOuter.PurchasedProduct state, Any any) {
+    throw new UnsupportedOperationException("Update handler for 'IgnoreOtherEvents' not implemented yet");
   }
 }
+
